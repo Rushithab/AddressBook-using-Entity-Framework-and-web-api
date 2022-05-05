395b430858eadd46e3b667405cd6db3061eafcd6
Rushithab <76553908+Rushithab@users.noreply.github.com>
2022-05-05 18:39:37 +0530

    Update IUsersService.cs

--- Services/IUsersService.cs
+++ Services/IUsersService.cs
@@ -6,7 +6,7 @@ namespace Addressbookapi.Services
     {
         public List<Contact> Get();
         public Contact Get(int id);
-        public Contact Create(Contact contact);
+        public int Create(Contact contact);
         public bool Update(Contact contact,int id);
         public bool Delete(int id);
     }
